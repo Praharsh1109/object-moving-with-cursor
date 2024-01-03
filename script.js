1cecca83c729b3d609c78d1542c013d16c5564cd
Praharsh Patel <ppraharsh11@gmail.com>
2024-01-03 10:09:44 -0500

    rectangle Done

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-var rectangle = document.querySelector('#rect')
+var rectangle = document.querySelector("#rect");
 
-rectangle.addEventListener('mousemove',function(go){
+var square = document.querySelector("#squa");
 
-    let movee = gsap.utils.mapRange(0,window.innerWidth,90,window.innerWidth-90,go.clientX)
-    gsap.to("#rect",{
-        left: movee,
-        // ease:Power3
-    });
-})
+rectangle.addEventListener("mousemove", function (data) {
+  let movee = gsap.utils.mapRange(
+    0,
+    window.innerWidth,
+    80,
+    window.innerWidth - 80,
+    data.clientX
+  );
+  gsap.to("#rect", {
+    left: movee,
+    ease: Power3
+  });
+});
 
-// rectangle.addEventListener('click',function(go){
-//     var rectangleLocation = rectangle.getBoundingClientRect(); 
-//     var insideRectLeft = go.clientX- rectangleLocation.left
-//       if (go.clientX < 150)  {
-//        var redColor=  gsap.utils.mapRange(0,rectangleLocation.width/2,255,0,insideRectLeft)
-//         gsap.to(rectangle,{
-//             backgroundColor: `rgb(${redColor},0,0)`,
-//             ease:Power4
-//         })
-//       }else{
-//         var greenColor=  gsap.utils.mapRange(rectangleLocation.width/2,rectangleLocation.width,0,255,insideRectLeft)
-//         gsap.to(rectangle,{
-//             backgroundColor: `rgb(0,${greenColor},0)`,
-//             ease:Power4
-//         })
-// })
-
-gsap.utils.mapRange();
+square.addEventListener("mousemove", function(go) {
+  console.log(go.clientY);
+  let upp = gsap.utils.mapRange(0, Window.innerHeight, 35, window.innerHeight - 20,go.clientY);
+  gsap.to("#squa",{
+    top:upp,
+    ease : Power3
+  })
+});
